1c80a0ce81309db80fdf93b0069214505ed3f513
Maciej Nowicki <maciej@nowicki.com>
2018-04-15 10:49:20 +0200

    Remove unused old code

--- src/main/scala/camerainterfacesystem/web/controllers/twirl/HiController.scala
+++ src/main/scala/camerainterfacesystem/web/controllers/twirl/HiController.scala
@@ -4,18 +4,15 @@ import java.time.Instant
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 
-import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import camerainterfacesystem.{Config, Main}
-import camerainterfacesystem.db.repos.{ImagesRepository, PresetsRepository}
-import camerainterfacesystem.db.util.PresetId
-import camerainterfacesystem.services.{CacheData, ImagesService}
-import camerainterfacesystem.utils.FunctionalUtils
+import camerainterfacesystem.Config
+import camerainterfacesystem.db.repos.ImagesRepository
+import camerainterfacesystem.services.ImagesService
 import camerainterfacesystem.web.controllers.rest.GoogleDriveController
 import camerainterfacesystem.web.{AppController, Controller}
 import com.typesafe.scalalogging.LazyLogging
-import html.{index, newestPresetList}
+import html.index
 
 @Controller
 class HiController extends AppController with LazyLogging {
@@ -47,45 +44,6 @@ class HiController extends AppController with LazyLogging {
               instantIsoDateFormatter.format(res._4.getOrElse(Instant.now())),
               googleAuthData
             ))
-          }
-        }
-      }
-    }
-  } ~ get {
-    path("preset" / IntNumber) {
-      (presetId) => {
-        parameters("hour".as[Int].?, "min".as[Long].?, "max".as[Long].?) {
-          (hourParam, minLong, maxLong) => {
-            val min = minLong.map(Instant.ofEpochMilli)
-            val max = maxLong.map(Instant.ofEpochMilli)
-
-            val future = for {
-              preset <- PresetsRepository.getPresetById(PresetId(presetId))
-              presetEither = Either.cond(preset.isDefined, preset.get, "Preset couldn't be found")
-              presetHours <- FunctionalUtils.reverseEitherFuture(presetEither.map(preset => PresetsRepository.getPresetsHours(preset.id)))
-              hour = presetHours.map(presetHours => hourParam.getOrElse(presetHours.head.hour))
-              images <- FunctionalUtils.reverseEitherFuture(hour.map(hour => {
-                ImagesService.getNewestForPresetHour(presetId, hour, min, max).map(_._2)
-              }))
-              sortedImages = images.map(images => images.sortWith((l, r) => l.phototaken.isAfter(r.phototaken)))
-            } yield (presetHours, hour, sortedImages, preset)
-
-            val normalizedFuture = future.map(x => {
-              x._1 match {
-                case Left(text) => Left(text)
-                case Right(value) => Right((value, x._2.right.get, x._3.right.get, x._4.get))
-              }
-            })
-
-            handleFutureError(onComplete(normalizedFuture)) {
-              case Left(text) => complete(HttpResponse(status = StatusCodes.Conflict, entity = text))
-              case Right(res) => {
-                htmlToResponseMarshalable {
-                  res._3.foreach(image => Main.imageDataService ! CacheData(image.fullpath))
-                  newestPresetList(hourParam.map(_ => res._3), res._4, res._2, res._1)
-                }
-              }
-            }
           }
         }
       }
