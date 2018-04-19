0ff86a4b465204aecd0e058873295532ab6d86f6
Maciej Nowicki <maciej@nowicki.com>
2018-04-20 00:53:35 +0200

    Set cache resources for 1 hour

--- src/main/scala/camerainterfacesystem/web/controllers/twirl/ResourcesController.scala
+++ src/main/scala/camerainterfacesystem/web/controllers/twirl/ResourcesController.scala
@@ -1,15 +1,20 @@
 package camerainterfacesystem.web.controllers.twirl
 
+import akka.http.scaladsl.model.headers.CacheDirectives
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import camerainterfacesystem.web.AppController
+import concurrent.duration._
 
-class  ResourcesController extends AppController {
-  override def route: Route = pathPrefix("web") {
-    getFromResourceDirectory("web", getClass.getClassLoader)
-  } ~ get {
-    pathPrefix("favicon.ico") {
-      getFromResource("web/favicon.ico")
+class ResourcesController extends AppController {
+  override def route: Route =
+    CacheDirectives.`max-age`((1 hour).toSeconds) {
+      pathPrefix("web") {
+        getFromResourceDirectory("web", getClass.getClassLoader)
+      } ~ get {
+        pathPrefix("favicon.ico") {
+          getFromResource("web/favicon.ico")
+        }
+      }
     }
-  }
 }
